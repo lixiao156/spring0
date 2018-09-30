1002e5c77188055a5d6691ffc6ebd452a3c43ad1
lixw <lixwcqs@163.com>
2018-09-30 08:57:47 +0800

    spring项目的配置文件

---
+++ src/main/java/com/cqs/learning/spring/AppConfig.java
@@ -0,0 +1,12 @@
+package com.cqs.learning.spring;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by li on 18-9-27.
+ */
+@Configuration
+@ComponentScan("com.cqs.learning")
+public class AppConfig {
+}
